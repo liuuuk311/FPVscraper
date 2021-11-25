10a135f8d7466a52a5a9fb27b395c0344c87bf0a
Luca Palonca <luca.palonca@sailogy.com>
2021-11-25 20:36:48 +0100

    Changed staticfiles settings

--- core/settings/base.py
+++ core/settings/base.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join("core", BASE_DIR, "staticfiles")
+STATIC_URL = '/core/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # CELERY STUFF
 BROKER_URL = 'redis://redis:6379'
