c112c90998a9c063abdcd13d16aee4f748b79660
Luca Palonca <luca.palonca@sailogy.com>
2021-11-28 18:08:00 +0100

    Adding selenium to get html

--- scraper/browser.py
+++ scraper/browser.py
@@ -1,10 +1,9 @@
-# from selenium import webdriver
+from selenium import webdriver
 
 
 def get_html(url: str) -> str:
-    # driver = webdriver.Chrome()
-    # driver.get(url)
-    # html = driver.page_source
-    # driver.close()
-    # return html
-    pass
+    driver = webdriver.Chrome()
+    driver.get(url)
+    html = driver.page_source
+    driver.close()
+    return html
